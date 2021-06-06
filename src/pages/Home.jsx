b1885ead9b5edc2f7bc0eb232d5bdc8e2c9ccacd
Ivan Bitkov <mribitkov@gmail.com>
2021-06-06 23:16:53 +0700

    Made "Hero" section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import '../scss/home.scss'
-import logo from '../img/logo.svg'
-import mailIcon from '../img/mail-icon.svg'
+import '../scss/home.scss';
+import logo from '../img/logo.svg';
+import mailIcon from '../img/mail-icon.svg';
+import heroImage from '../img/hero-image.png';
+import arrowRightWhite from '../img/arrow-right-white.svg'
 
 function Home() {
     return (
@@ -20,6 +22,23 @@ function Home() {
                     </nav>
                 </div>
             </header>
+            <main>
+                <section className="hero">
+                    <div className="wrapper">
+                        <div className="hero__container">
+                            <div className="text">
+                                <h3 className="subtitle">Ingenieur Dieter Weiss</h3>
+                                <h1 className="title">Ingenieurbüro für Bau- und Vermessungswesen</h1>
+                                <p className="desc">Wenn Sie zuverlässige Ingenieure brauchen – das Ingenieurbüro Dieter Weiss ist Garant für den Erfolg Ihrer Projekte.</p>
+                                <Link to="/" className="button">Ihr Projekt mit uns <img src={arrowRightWhite} alt=""/></Link>
+                            </div>
+                            <div className="image">
+                                <img src={heroImage} alt=""/>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 }
