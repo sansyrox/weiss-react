b22e3f40e6a86597d5889ef0608bdd5f5a57e5cb
Ivan Bitkov <mribitkov@gmail.com>
2021-06-07 02:50:49 +0700

    Made "History" section.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ import serviceImg1 from '../img/service-image-1.png';
 import serviceImg2 from '../img/service-image-2.png';
 import serviceImg3 from '../img/service-image-3.png';
 import arrowRightOrange from '../img/arrow-right-orange.svg';
-
+import historyImage from '../img/history-image.png';
 
 
 function Home() {
@@ -44,32 +44,52 @@ function Home() {
                         </div>
                     </div>
                 </section>
-                <section className="service">
-                    <div className="wrapper">
-                        <h2 className="service__title">Unsere Dienstleistungen</h2>
-                        <p className="service__desc">Dienstleistung ist für unsere Mitarbeiter Herausforderung, das ist die Stärke unseres Teams. Ganz gleich, ob Verkehrsplanungen, Ver- und Ent­sorgung, Lärmschutz oder Spezialgebiete – wir bieten Rundum-Service mit:</p>
-                        <div className="service__container">
-                            <div className="service__item">
-                                <img src={serviceImg1} className="preview" alt="Studien und Konzepte, Gesamtplanung"/>
-                                <h3>Studien und Konzepte, Gesamtplanung</h3>
-                                <p>Neubau und Ausbau von Straßen, Verkehrsflächen, Wasserwegen und Eisenbahnen erfordert überzeugende Verkehrskonzepte.</p>
-                                <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
-                            </div>
-                            <div className="service__item">
-                                <img src={serviceImg2} className="preview" alt="Verkehrsanlagen und Ingenieurbauwerke"/>
-                                <h3>Verkehrsanlagen und Ingenieurbauwerke</h3>
-                                <p>Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss hat deshalb ein Expertenteam aufgebaut.</p>
-                                <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
-                            </div>
-                            <div className="service__item">
-                                <img src={serviceImg3} className="preview" alt="Entwurfsvermessung, 3D-Laserscanning"/>
-                                <h3>Entwurfsvermessung, 3D-Laserscanning</h3>
-                                <p>Bei Ausführung sämtlicher vermessungstechnischer Aufgaben im Leistungsbild „Entwurfsvermessung“ sind wir ein leistungsstarker Partner.</p>
-                                <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
+                <div className="service-and-history">
+                    <section className="service">
+                        <div className="wrapper">
+                            <h2 className="service__title">Unsere Dienstleistungen</h2>
+                            <p className="service__desc">Dienstleistung ist für unsere Mitarbeiter Herausforderung, das ist die Stärke unseres Teams. Ganz gleich, ob Verkehrsplanungen, Ver- und Ent­sorgung, Lärmschutz oder Spezialgebiete – wir bieten Rundum-Service mit:</p>
+                            <div className="service__container">
+                                <div className="service__item">
+                                    <img src={serviceImg1} className="preview" alt="Studien und Konzepte, Gesamtplanung"/>
+                                    <h3>Studien und Konzepte, Gesamtplanung</h3>
+                                    <p>Neubau und Ausbau von Straßen, Verkehrsflächen, Wasserwegen und Eisenbahnen erfordert überzeugende Verkehrskonzepte.</p>
+                                    <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
+                                </div>
+                                <div className="service__item">
+                                    <img src={serviceImg2} className="preview" alt="Verkehrsanlagen und Ingenieurbauwerke"/>
+                                    <h3>Verkehrsanlagen und Ingenieurbauwerke</h3>
+                                    <p>Verkehrskonzepte sind ein entscheidender Faktor jeglicher Infrastruktur. Das Ingenieurbüro Weiss hat deshalb ein Expertenteam aufgebaut.</p>
+                                    <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
+                                </div>
+                                <div className="service__item">
+                                    <img src={serviceImg3} className="preview" alt="Entwurfsvermessung, 3D-Laserscanning"/>
+                                    <h3>Entwurfsvermessung, 3D-Laserscanning</h3>
+                                    <p>Bei Ausführung sämtlicher vermessungstechnischer Aufgaben im Leistungsbild „Entwurfsvermessung“ sind wir ein leistungsstarker Partner.</p>
+                                    <Link to="/">Erfahren Sie mehr <img src={arrowRightOrange} alt="arrowRight"/></Link>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
+                    <section className="history">
+                        <div className="wrapper">
+                            <div className="history__container">
+                                <div className="history__text">
+                                    <div className="container">
+                                        <h4 className="subtitle">Unsere Firmenhistorie</h4>
+                                        <h3 className="title">Diplom-Ingenieur Dieter Weiss gründete im Jahre 1971 sein Ingenieurbüro.</h3>
+                                        <p className="desc">Fast 30 Jahre lang führte er es als Alleininhaber. 1998 trat Sohn Klaus weiss als Stellvertreter an die Seite seines Vaters. Seit 2006 ist Diplom-Ingenieur Klaus Weiss alleiniger Geschäftsführer</p>
+                                        <span className="line"></span>
+                                        <Link to="/" className="link">Lesen Sie weiter <img src={arrowRightWhite} alt="arrowRight"/></Link>
+                                    </div>
+                                </div>
+                                <div className="history__image">
+                                    <img src={historyImage} alt="Diplom-Ingenieur Dieter Weiss gründete im Jahre 1971 sein Ingenieurbüro."/>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
             </main>
         </div>
     );
